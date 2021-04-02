f57e6612243513352c13a989d3e63426a7f74215
gjspencer22 <gjspencer22@gmail.com>
2021-04-02 02:05:12 -0600

    10.2 done

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,6 +6,25 @@ console.log(new Potion());
 
 const player = require('../lib/Player');
 
-expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
+// expect(player.inventory).toEqual(
+//     expect.arrayContaining([expect.any(Object)])
+//   );
+
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
